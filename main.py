c9e84171c4bf5873c987ee40d9bb172ccd6adcd7
felanny <felamin@163.com>
2025-09-25 15:45:44 +0800

    revert: url

--- main.py
+++ main.py
@@ -119,7 +119,7 @@ class MiMotionRunner:
     # 登录
     def login(self):
 
-        url1 = "https://api-user.zepp.com/v2/registrations/" + self.user + "/tokens"
+        url1 = "https://api-user.huami.com/registrations/" + self.user + "/tokens"
         login_headers = {
             "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
             "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 14_7_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2",
